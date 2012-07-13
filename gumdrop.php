3b452edc4a87dcfa2f20b4b677f1931e58161cd5
Simon Jodet <simon.jodet@f-secure.com>
2012-07-13 15:47:46 +0200

    Adding the CLI loader / Updating the composer autoloader

---
+++ gumdrop.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+$source = realpath($_SERVER['argv'][1]) . '/';
+$destination = realpath($_SERVER['argv'][2]) . '/';
+if ($source == '/' || $source === false)
+{
+    echo 'Given source path is not valid!' . PHP_EOL;
+    exit(1);
+}
+if ($destination == '/' || $destination === false)
+{
+    echo 'Given destination path is not valid!' . PHP_EOL;
+    exit(1);
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+$Application = new \Gumdrop\Application();
+$Application->setMarkdownParser(new \dflydev\markdown\MarkdownParser());
+$Application->setFileHandler(new \Gumdrop\FileHandler());
+$Application->setMarkdownFilesHandler(new \Gumdrop\MarkdownFilesHandler($Application));
+$Application->generate($source, $destination);
+echo $source . PHP_EOL . $destination . PHP_EOL;
