d7908f5478e27300ff7c93039295a4300e88e4a1
Simon Jodet <simon.jodet@f-secure.com>
2012-07-19 15:13:15 +0200

    Adding a Configuration class

---
+++ Gumdrop/Configuration.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gumdrop;
+
+/**
+ * Configuration container
+ */
+class Configuration
+{
+
+    /**
+     * @var object
+     */
+    private $configuration;
+
+    /**
+     * @param string $location
+     */
+    public function __construct($location)
+    {
+        $location = $location . '/conf.json';
+        if (!file_exists($location))
+        {
+            throw new \Exception('Could not find the configuration file at ' . $location);
+        }
+
+        $this->configuration = json_decode(file_get_contents($location));
+        if (json_last_error() != JSON_ERROR_NONE)
+        {
+            throw new \Exception('Invalid configuration in ' . $location);
+        }
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        if (!isset($this->configuration->$key))
+        {
+            return null;
+        }
+        return $this->configuration->$key;
+    }
+}
