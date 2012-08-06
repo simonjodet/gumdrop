6da2f2693f83416ba1026787e2ba5119760ef2a6
Simon Jodet <simon.jodet@f-secure.com>
2012-08-06 13:26:29 +0200

    Page class: putting accessors next to their property

--- Gumdrop/Page.php
+++ Gumdrop/Page.php
@@ -8,19 +8,72 @@ namespace Gumdrop;
 class Page
 {
     /**
+     * @var \Gumdrop\Application
+     */
+    private $app;
+
+    /**
      * @var string
      */
     private $location;
 
     /**
+     * @param string string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
      * @var string
      */
-
     private $markdownContent;
+
+    /**
+     * @param string $markdownContent
+     */
+    public function setMarkdownContent($markdownContent)
+    {
+        $this->markdownContent = $markdownContent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMarkdownContent()
+    {
+        return $this->markdownContent;
+    }
+
     /**
      * @var string
      */
     private $htmlContent;
+
+    /**
+     * @param string $htmlContent
+     */
+    public function setHtmlContent($htmlContent)
+    {
+        $this->htmlContent = $htmlContent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHtmlContent()
+    {
+        return $this->htmlContent;
+    }
 
     /**
      * @var \Gumdrop\PageConfiguration
@@ -28,9 +81,42 @@ class Page
     private $configuration;
 
     /**
+     * @param \Gumdrop\PageConfiguration $configuration
+     */
+    public function setConfiguration(\Gumdrop\PageConfiguration $configuration)
+    {
+        $this->setMarkdownContent($configuration->extractHeader($this->getMarkdownContent()));
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * @return \Gumdrop\PageConfiguration
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    /**
      * @var \Gumdrop\PageCollection
      */
     private $collection;
+
+    /**
+     * @param \Gumdrop\PageCollection $collection
+     */
+    public function setCollection(\Gumdrop\PageCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * @return \Gumdrop\PageCollection
+     */
+    public function getCollection()
+    {
+        return $this->collection;
+    }
 
     /**
      * @var \Twig_Environment
@@ -38,15 +124,43 @@ class Page
     private $layoutTwigEnvironment;
 
     /**
+     * @param \Twig_Environment $layoutTwigEnvironment
+     */
+    public function setLayoutTwigEnvironment(\Twig_Environment $layoutTwigEnvironment)
+    {
+        $this->layoutTwigEnvironment = $layoutTwigEnvironment;
+    }
+
+    /**
+     * @return \Twig_Environment
+     */
+    public function getLayoutTwigEnvironment()
+    {
+        return $this->layoutTwigEnvironment;
+    }
+
+    /**
      * @var \Twig_Environment
      */
     private $pageTwigEnvironment;
 
     /**
-     * @var \Gumdrop\Application
+     * @param \Twig_Environment $pageTwigEnvironment
      */
-    private $app;
+    public function setPageTwigEnvironment($pageTwigEnvironment)
+    {
+        $this->pageTwigEnvironment = $pageTwigEnvironment;
+    }
 
+    /**
+     * @return \Twig_Environment
+     */
+    public function getPageTwigEnvironment()
+    {
+        return $this->pageTwigEnvironment;
+    }
+
+    /* METHODS */
     /**
      * @param \Gumdrop\Application $app
      */
@@ -54,8 +168,6 @@ class Page
     {
         $this->app = $app;
     }
-
-    /* METHODS */
 
     /**
      * Converts the Markdown code to HTML
@@ -123,120 +235,5 @@ class Page
                 'page' => $this->getConfiguration(),
                 'pages' => $this->getCollection()
             );
-    }
-
-    /* ACCESSORS */
-
-    /**
-     * @param string $htmlContent
-     */
-    public function setHtmlContent($htmlContent)
-    {
-        $this->htmlContent = $htmlContent;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHtmlContent()
-    {
-        return $this->htmlContent;
-    }
-
-    /**
-     * @param string string $location
-     */
-    public function setLocation($location)
-    {
-        $this->location = $location;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLocation()
-    {
-        return $this->location;
-    }
-
-    /**
-     * @param string $markdownContent
-     */
-    public function setMarkdownContent($markdownContent)
-    {
-        $this->markdownContent = $markdownContent;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMarkdownContent()
-    {
-        return $this->markdownContent;
-    }
-
-    /**
-     * @param \Gumdrop\PageConfiguration $configuration
-     */
-    public function setConfiguration(\Gumdrop\PageConfiguration $configuration)
-    {
-        $this->setMarkdownContent($configuration->extractHeader($this->getMarkdownContent()));
-        $this->configuration = $configuration;
-    }
-
-    /**
-     * @return \Gumdrop\PageConfiguration
-     */
-    public function getConfiguration()
-    {
-        return $this->configuration;
-    }
-
-    /**
-     * @param \Gumdrop\PageCollection $collection
-     */
-    public function setCollection(\Gumdrop\PageCollection $collection)
-    {
-        $this->collection = $collection;
-    }
-
-    /**
-     * @return \Gumdrop\PageCollection
-     */
-    public function getCollection()
-    {
-        return $this->collection;
-    }
-
-    /**
-     * @param \Twig_Environment $layoutTwigEnvironment
-     */
-    public function setLayoutTwigEnvironment(\Twig_Environment $layoutTwigEnvironment)
-    {
-        $this->layoutTwigEnvironment = $layoutTwigEnvironment;
-    }
-
-    /**
-     * @return \Twig_Environment
-     */
-    public function getLayoutTwigEnvironment()
-    {
-        return $this->layoutTwigEnvironment;
-    }
-
-    /**
-     * @param \Twig_Environment $pageTwigEnvironment
-     */
-    public function setPageTwigEnvironment($pageTwigEnvironment)
-    {
-        $this->pageTwigEnvironment = $pageTwigEnvironment;
-    }
-
-    /**
-     * @return \Twig_Environment
-     */
-    public function getPageTwigEnvironment()
-    {
-        return $this->pageTwigEnvironment;
     }
 }
