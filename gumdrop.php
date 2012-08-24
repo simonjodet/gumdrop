c59e1244372363fc70d3d8f15719c66bb158ba83
Simon Jodet <simon.jodet@f-secure.com>
2012-08-24 15:02:53 +0200

    Auto-refresh and built-in web server options available through bin/gumdrop

--- gumdrop.php
+++ gumdrop.php
@@ -2,17 +2,16 @@
 <?php
 $source = realpath($_SERVER['argv'][1]) . '/';
 $destination = realpath($_SERVER['argv'][2]) . '/';
-if ($source == '/' || $source === false)
+if ($source == '/' || $source == realpath('.') . '/' || $source === false)
 {
     echo 'Given source path is not valid!' . PHP_EOL;
     exit(1);
 }
-if ($destination == '/' || $destination === false)
+if ($destination == '/' || $destination == realpath('.') . '/' || $destination === false)
 {
     echo 'Given destination path is not valid!' . PHP_EOL;
     exit(2);
 }
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 $Application = new \Gumdrop\Application();
@@ -27,5 +26,20 @@ $Application->setTwigFileHandler(new \Gumdrop\TwigFileHandler($Application));
 $Application->setEngine(new \Gumdrop\Engine($Application));
 
 $Application->getEngine()->run();
-echo 'Gumdrop converted your MarkDown files converted to ' . $destination . PHP_EOL;
+echo 'Gumdrop converted your MarkDown files to ' . $destination . PHP_EOL;
+if ($_SERVER['argv'][3] == '-r')
+{
+    $last_date = $Application->getFileHandler()->getLatestFileDate();
+    while (true)
+    {
+        $date = $Application->getFileHandler()->getLatestFileDate();
+        if ($last_date < $date)
+        {
+            $Application->getEngine()->run();
+            echo 'Gumdrop converted your MarkDown files to ' . $destination . PHP_EOL;
+            $last_date = $date;
+        }
+        sleep(2);
+    }
+}
 exit(0);
