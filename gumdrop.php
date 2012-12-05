86351e1abe9ef5f57693f0adc6efed266eec62a8
Simon Jodet <simon@jodet.com>
2012-12-05 17:38:20 +0100

    Gumdrop can be declared as composer dependency

--- gumdrop.php
+++ gumdrop.php
@@ -12,7 +12,19 @@ if ($destination == '/' || $destination == realpath(__DIR__) . '/' || $destinati
     echo 'Given destination path is not valid!' . PHP_EOL;
     exit(2);
 }
-require_once __DIR__ . '/vendor/autoload.php';
+if (file_exists(__DIR__ . '/vendor/autoload.php'))
+{
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+elseif (file_exists(__DIR__ . '/../../autoload.php'))
+{
+    require_once __DIR__ . '/../../autoload.php';
+}
+else
+{
+    echo 'Gumdrop could not find dependencies' . PHP_EOL;
+    exit(1);
+}
 
 $Application = new \Gumdrop\Application();
 
